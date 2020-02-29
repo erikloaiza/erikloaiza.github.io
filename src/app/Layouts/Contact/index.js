efde0a59abbf273152929a04d0a6ab43651e8009
ERIK <erik.loaiza@uao.edu.co>
2020-02-29 10:42:42 -0500

    fixed: styles neomorph

--- src/app/Layouts/Contact/index.js
+++ src/app/Layouts/Contact/index.js
@@ -7,7 +7,7 @@ function Contact(){
             <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
-          <form className="d-flex flex-column p-4" action="mailto:erik.loaiza.p@gmail.com" method="post" enctype="text/plain">
+          <form className="d-flex flex-column p-4" action="mailto:erik.loaiza.p@gmail.com" method="post" encType="text/plain">
               <label className="mt-2">subject</label>
               <input/>
               <label className="mt-2">email or contact</label>
