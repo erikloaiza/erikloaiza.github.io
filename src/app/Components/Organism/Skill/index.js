9ec2db6d7af09849187c025a36795c8cb4c2667a
ERIK <erik.loaiza@uao.edu.co>
2020-02-29 10:25:24 -0500

    update: progressbar animation

--- src/app/Components/Organism/Skill/index.js
+++ src/app/Components/Organism/Skill/index.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import VisibilitySensor from "react-visibility-sensor";
+
 import "react-circular-progressbar/dist/styles.css";
+
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 function Skill() {
   const percentage = 66;
   const skill = "Skill";
   return (
     <div className="col-5 col-md-3 col-lg-2 p-3 skill my-2 mx-2">
-      <CircularProgressbar
-        value={percentage}
-        text={skill}
-        circleRatio="0.75"
-        strokeWidth="4"
-        className="skill--detail"
-        styles={buildStyles({
-          rotation: 1 / 2 + 1 / 8
-        })}
-      />
+      <VisibilitySensor>
+        {({ isVisible }) => {
+          const value = isVisible ? percentage : 0;
+          return (
+            <CircularProgressbar
+              value={value}
+              text={skill}
+              circleRatio="0.75"
+              strokeWidth="4"
+              className="skill--detail"
+              styles={buildStyles({
+                rotation: 1 / 2 + 1 / 8,
+                pathTransitionDuration: 0.5
+              })}
+            />
+          );
+        }}
+      </VisibilitySensor>
     </div>
   );
 }
