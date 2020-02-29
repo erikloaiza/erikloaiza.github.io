e1590a5b5f357f6cb598f9189b02f7eba4084c51
ERIK <erik.loaiza@uao.edu.co>
2020-02-29 10:57:46 -0500

    update: font

--- src/app/Components/Organism/Education/index.js
+++ src/app/Components/Organism/Education/index.js
@@ -6,7 +6,7 @@ function Education() {
             <div className="row education">
                 <i className="icon">A</i>
                 <div className="col mx-2 d-flex justify-content-between align-items-center info px-4">
-                    <h6 className="m-0">Lorem Ipsum</h6>
+                    <b className="m-0">Lorem Ipsum</b>
                     <span>2000-2000</span>
                 </div>
             </div>
