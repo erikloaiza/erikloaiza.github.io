da0bbfcad1ec57b05b08c2bae581081c32d600ab
ERIK <erik.loaiza@uao.edu.co>
2020-03-23 20:18:37 -0500

    build

---
+++ build/precache-manifest.1076424a98137fb3c518f45e769d4429.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd137aca8c92008cfb71d14c3fb782d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d48591133158c4fb905",
+    "url": "/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "26986a75533e1766d48e",
+    "url": "/static/css/main.6fecf34c.chunk.css"
+  },
+  {
+    "revision": "0d48591133158c4fb905",
+    "url": "/static/js/2.4cd9dfc9.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.4cd9dfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26986a75533e1766d48e",
+    "url": "/static/js/main.aa071c99.chunk.js"
+  },
+  {
+    "revision": "42bb9a9a3bff69bebad0",
+    "url": "/static/js/runtime-main.50115ae8.js"
+  }
+]);
