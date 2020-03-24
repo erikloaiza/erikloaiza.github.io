799e49d090bb4c3c68ae579346501361c08ee49d
ERIK <erik.loaiza@uao.edu.co>
2020-03-23 20:17:32 -0500

    update: finished

--- src/app/Layouts/Main/index.js
+++ src/app/Layouts/Main/index.js
@@ -4,7 +4,7 @@ import {
   Route,
   Redirect,
   Switch,
-  NavLink
+  NavLink,
 } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
@@ -14,6 +14,8 @@ import Experience from "../Experience";
 import Portfolio from "../Portfolio";
 import Contact from "../Contact";
 import SideNav from "../../Components/Organism/SideNav";
+
+import { FaFacebook, FaTwitter, FaLinkedin, FaGithub, FaStackOverflow} from "react-icons/fa";
 
 function Main() {
   return (
@@ -40,6 +42,23 @@ function Main() {
               Contact
             </NavLink>
           </Nav>
+          <div className="col row my-4 social-links ml-2">
+            <a href="https://web.facebook.com/erik.f.loaiza" className="col-1">
+              <FaFacebook />
+            </a>
+            <a href="https://twitter.com/erikferloaiza" className="col-1">
+              <FaTwitter />
+            </a>
+            <a href="https://www.linkedin.com/in/erikloaiza/" className="col-1">
+              <FaLinkedin />
+            </a>
+            <a href="https://github.com/erikloaiza/" className="col-1">
+              <FaGithub />
+            </a>
+            <a href="https://stackoverflow.com/users/12957088/erik-fernando-loaiza-pati%c3%b1o" className="col-1">
+              <FaStackOverflow />
+            </a>
+          </div>
         </SideNav>
         <div className="col p-0 pr-md-5 mr-md-5">
           <div className="container">
